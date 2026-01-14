082e3f766cd971153cb253f7e631095d8f012a71
Jussi Kinnula <jussi.kinnula@gmail.com>
2026-01-14 19:08:50 +0200

    Don't show zero price and quantities + show <100 quantity as is

--- src/utils/formatted-quantity.ts
+++ src/utils/formatted-quantity.ts
@@ -4,16 +4,16 @@ export function formattedQuantity(input?: string) {
   }
 
   const value = Number(input);
+  if (value === 0) {
+    return "";
+  }
+
   if (value >= 1000000) {
     return `>1000k`;
   }
 
   if (value >= 1000) {
     return `${Math.round(value / 1000)}k`;
-  }
-
-  if (value <= 100) {
-    return "<100";
   }
 
   return input;
